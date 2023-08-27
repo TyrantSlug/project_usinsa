57faa03dd708a1b9e0e6a2f050c6b729adc215f6
TyrantSlug <tyrant_slug@naver.com>
2023-08-28 00:05:20 +0900

    오더서비스 수정, 프론트 수정3

--- umsfront/src/component/member/MemberOrderList.js
+++ umsfront/src/component/member/MemberOrderList.js
@@ -13,9 +13,8 @@ function MemberOrderList() {
   useEffect(() => {
     fetchFn(
       "GET",
-      `/api/order-service/orders/list/${username}?pageNum=0`,
-      null
-    ).then((data) => {
+      `/api/order-service/orders/list/${username}?pageNum=0`
+).then((data) => {
       if (LOGINER == username || ROLE == 2) {
         setPageList(data.result.content);
       } else {
